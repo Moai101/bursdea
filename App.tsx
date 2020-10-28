05031864f97b9cb0c6122f9996eb9ae0d0013097
服部明諭 <thunaotoko@gmail.com>
2020-10-28 11:24:14 +0900

    add react-navigation

--- App.tsx
+++ App.tsx
@@ -1,15 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up !</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+
+const Stack = createStackNavigator();
+
+
+
+export default class App extends React.Component {
+
+  render(){
+    return (
+      <View style={styles.container}>
+
+        <Text>Open!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+
+  }
+
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +64,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
